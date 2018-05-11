1a3a285aa0540018addae9a9646a33da9261b242
burkaans <raimonds.buholts@gmail.com>
2018-05-11 20:41:14 +0300

    day_6
    1.commit

--- day_6/day_6/Program.cs
+++ day_6/day_6/Program.cs
@@ -10,6 +10,17 @@ namespace day_6
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 4; i++) //i=0 - saakums, i<4 - i ir liidz 3, i++ - i=i+1(shaadi arii der), jaaieveero seciiba!
+            {
+                Console.WriteLine(i);
+            }
+            int skaititajs = 0;
+            while (skaititajs != 5)
+            {
+                Console.WriteLine(skaititajs);
+                skaititajs++;
+            }
+            Console.ReadLine();
         }
     }
 }
